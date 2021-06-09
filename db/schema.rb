5b3da6db92ee460f5091422aa45ee57b5570b237
Applepie9 <horel.dora@gmail.com>
2021-06-09 10:30:59 +0200

    Bug fix for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_08_122919) do
+ActiveRecord::Schema.define(version: 2021_06_09_082610) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
