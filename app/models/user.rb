9c69f59397a6393a61b76e1c9843c48573795e64
Applepie9 <horel.dora@gmail.com>
2021-07-27 18:07:37 +0200

    Add password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -65,8 +65,8 @@ end
   # Sets the password reset attributes.
   def create_reset_digest
     self.reset_token = User.new_token
-    update_attribute(:reset_digest,  User.digest(reset_token))
-    update_attribute(:reset_sent_at, Time.zone.now)
+    update_columns(reset_digest:  User.digest(reset_token),
+                   reset_sent_at: Time.zone.now)
   end
 
   # Sends password reset email.
@@ -74,6 +74,10 @@ end
     UserMailer.password_reset(self).deliver_now
   end
 
+  # Returns true if a password reset has expired.
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
 
   private
 
